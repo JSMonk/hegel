9ecdef7e5fb2aeee9978c256729e93670e966e23
Artem Kobzar <dendishiz0@gmail.com>
2019-06-09 16:07:48 +0300

    chore: npm publish.

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,7 +1,7 @@
+import chalk from "chalk";
 import { join } from "path";
 import { parse } from "@babel/parser";
 import { readFileSync } from "fs";
-import chalk from "chalk";
 import { codeFrameColumns } from "@babel/code-frame";
 import createTypeGraph from "@hegel/core/type-graph/type-graph";
 import printError from "./printer";
@@ -16,7 +16,10 @@ const babelrc = {
   const fileContent = readFileSync(path, "utf8");
   const ast = parse(fileContent, babelrc);
   const [modules, errors] = await createTypeGraph([{ ...ast.program, path }], () => {});
+  let result = ""; 
   for (const error of errors) {
-    console.log(printError(error, fileContent));
+    result = `${result}\n\n${printError(error, fileContent)}`;
   }
+  console.log(result);
+  process.exit(errors.length);
 })()
--- packages/cli/src/test.js
+++ packages/cli/src/test.js
@@ -1,1 +1,13 @@
-const a: number = "2";
+type User = { id: number };
+
+const user: ?User = { id: 0 };
+
+if (user) {}
+
+for (let i = 10; i--;) {}
+
+while(user) {}
+
+if ("b" in 2) {}
+
+if (typeof user === "number") {}
