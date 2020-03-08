8b913a950d3a8e1d757c4efe913946c29bb92045
Artem Kobzar <dendishiz0@gmail.com>
2020-03-08 16:37:06 +0200

    chore: bump versions.

--- packages/cli/src/lib/file-system.js
+++ packages/cli/src/lib/file-system.js
@@ -3,8 +3,8 @@ import { join } from "path";
 import type { Config } from "./config";
 
 export function getSources(config: Config) {
-  const include: Array<string> = config.include || [];
-  const exclude: Array<string> = config.exclude || [];
+  const include = config.include || [];
+  const exclude = config.exclude || [];
   return glob.sync(include.join(), {
     cwd: config.workingDirectory,
     dot: true,
