fee95e847cb0792c6c556d4d7d97bea4965c8939
JSMonk <dendishiz0@gmail.com>
2020-03-14 14:05:47 +0000

    Deploying to gh-pages from  - c26b6f654f003c2222edaee826397f8f5002d251 🚀

---
+++ component---src-pages-404-js-5620ef8a49a8ad47c12d.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([[28],{1322:function(t,e,n){"use strict";n.r(e);n(22);var i=n(36);e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5620ef8a49a8ad47c12d.js.map
