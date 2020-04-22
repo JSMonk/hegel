20aa1f077417ed172570bf06959a5c0cc2f08d60
Sergii Kostyrko <srg.kostyrko@gmail.com>
2020-04-22 10:40:39 +0300

    build: use local version of core package in docs dev mode

--- packages/docs/gatsby-node.js
+++ packages/docs/gatsby-node.js
@@ -1,11 +1,20 @@
 const fs = require("fs");
+const path = require("path");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+const { NODE_ENV } = process.env;
 
 const REPOSITORY = "https://github.com/JSMonk/hegel";
 const STD_LIB_PATH = "@hegel/typings/standard/index.d.ts";
 const STD_LIB_CONTENT = fs
   .readFileSync(require.resolve(STD_LIB_PATH), "utf8")
   .replace(/`/g, "");
+
+const resolve = NODE_ENV !== 'production' ?  {
+  alias: {
+    '@hegel/core': path.resolve(__dirname, '../../core/build/'),
+  },
+} : {};
 
 exports.onCreateWebpackConfig = args => {
   args.actions.setWebpackConfig({
@@ -17,6 +26,7 @@ exports.onCreateWebpackConfig = args => {
       new MonacoWebpackPlugin({
         languages: ["javascript", "css", "html", "typescript", "json"]
       })
-    ]
+    ],
+    resolve
   });
 };
