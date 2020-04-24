9df9446035ae7150fda230ad403e9fac59eb5b44
Demetri0 <d3i0@ya.ru>
2020-04-25 02:30:19 +0600

    add jsdoc @throws for JSON.stringify

--- packages/typings/standard/index.d.ts
+++ packages/typings/standard/index.d.ts
@@ -1603,6 +1603,7 @@ interface JSON {
   //       * @param value A JavaScript value, usually an object or array, to be converted.
   //       * @param replacer A function that transforms the results.
   //       * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+  //       * @throws {TypeError} because of hegel could not detect circular dependency inside objects
   //       */
   stringify(
     value: any,
@@ -1614,6 +1615,7 @@ interface JSON {
   //       * @param value A JavaScript value, usually an object or array, to be converted.
   //       * @param replacer An array of strings and numbers that acts as a approved list for selecting the object properties that will be stringified.
   //       * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+  //       * @throws {TypeError} because of hegel could not detect circular dependency inside objects
   //       */
   stringify(
     value: any,
